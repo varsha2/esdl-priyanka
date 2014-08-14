38e22646eabaf970dda3f5d5f884a27bb666e555
varsha2 <varsha.patole@cummiscollege.in>
2014-08-14 10:42:52 +0530

    Update Calc.cpp

--- Calc.cpp
+++ Calc.cpp
@@ -5,11 +5,11 @@
  {
  cout << "welcome to github";
  }
-double square(double in_number)
+double square(double in_number)//square function
 {
  return in_number * in_number;
 }
-double cube(double in_number)
+double cube(double in_number)//cube function
 {
  return in_number * in_number * in_number;
 }
